d58459f1c3e413c70e3ecd77aaa4038b35b5feab
SKel1817 <vnielson@oakland.edu>
2025-03-18 15:38:00 -0400

    added some comments to main

--- Main.java
+++ Main.java
@@ -1,2 +1,18 @@
 //import all files in directory
 
+public class Main {
+    public static void main(String[] args) {
+        ECC ecc = new ECC();
+        RSA rsa = new RSA();
+        TDES tdes = new TDES();
+        AES aes = new AES();
+        DES des = new DES();
+        File file = new File();
+    }
+    // take the file name as input
+    // read the file
+    // encrypt the file with all the algorithms
+    // decrypt the file with all the algorithms
+    // create report on speed, reliability, and security
+
+}
